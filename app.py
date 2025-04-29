129d41b721aa40499174e70c678001fdc2eb71e5
mithun-k04 <mithunkaruppusamy3@gmail.com>
2025-04-29 17:41:36 +0530

    Initial Commit

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+import urllib.parse
+
+app = Flask(__name__)
+
+@app.route('/get-directions', methods=['POST'])
+def get_directions():
+    data = request.get_json()
+    origin_lat = data['origin']['lat']
+    origin_lng = data['origin']['lng']
+    destination = urllib.parse.quote(data['destination'])  
+
+    map_url = f"https://www.google.com/maps/dir/?api=1&origin={origin_lat},{origin_lng}&destination={destination}&travelmode=driving"
+
+    return jsonify({'url': map_url})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
